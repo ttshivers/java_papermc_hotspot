81a483b64a288668bf769e342f65be31932d217e
Travis Shivers <ttshivers@gmail.com>
2020-06-30 13:46:32 -0500

    Initial files

---
+++ README.md
@@ -0,0 +1,2 @@
+# java_papermc_hotspot
+Java PaperMC Container with Hotspot
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd /home/container
+
+# Output Current Java Version
+java -version
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+export NURSERY_MINIMUM=$(($SERVER_MEMORY / 2))
+export NURSERY_MAXIMUM=$(($SERVER_MEMORY * 4 / 5))
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+curl -o server.jar "https://papermc.io/api/v1/paper/1.16.1/latest/download"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
